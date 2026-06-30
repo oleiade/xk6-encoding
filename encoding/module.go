167be16e3d665ab08d4a4452c48446b8122adafd
junya koyama <arukiidou@yahoo.co.jp>
2026-07-01 00:16:11 +0900

    Fix: encoding\module.go:51:43: any: interface{} can be replaced by any (modernize)
    Signed-off-by: junya koyama <arukiidou@yahoo.co.jp>

--- encoding/module.go
+++ encoding/module.go
@@ -48,7 +48,7 @@ func (*RootModule) NewModuleInstance(vu modules.VU) modules.Instance {
 // Exports implements the modules.Instance interface and returns
 // the exports of the JS module.
 func (mi *ModuleInstance) Exports() modules.Exports {
-	return modules.Exports{Named: map[string]interface{}{
+	return modules.Exports{Named: map[string]any{
 		"TextDecoder": mi.NewTextDecoder,
 		"TextEncoder": mi.NewTextEncoder,
 	}}
